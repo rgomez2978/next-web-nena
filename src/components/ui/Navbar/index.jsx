a14cee0a4ae2391708f69bab58a538abfe97abc5
Ronald Gomez <ronald.gomez@dronena.com>
2024-07-18 13:40:59 -0400

    add export dist

--- src/components/ui/Navbar/index.jsx
+++ src/components/ui/Navbar/index.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
         <div className={`${styles.sidebar_section}`}>
           <Link href={'/'} >
             <Image
-              src="./images/logos/logo.png"
+              src="images/logos/logo.png"
               alt="logo nena"
               width={200}
               height={100}
@@ -64,7 +64,7 @@ const Navbar = () => {
                   className={`${styles.li_link}`}>
                   {name === 'logo' ? (
                     <Image
-                      src="./images/logos/logo.png"
+                      src="images/logos/logo.png"
                       alt="logo nena"
                       width={200}
                       height={100}
