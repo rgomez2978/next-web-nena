e3c1d9ef24f5a897081171054410011f418c5c73
Ronald Gomez <ronald.gomez@dronena.com>
2024-07-18 14:39:39 -0400

    add export dist

--- src/components/shared/Title/index.jsx
+++ src/components/shared/Title/index.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
 import React, { useEffect, useState } from 'react'
-
+import styles from "./style.module.scss";
 
 const TitleComp = ({ title, description, type }) => {
   const [activeCicle, setActiveCicle] = useState(false)
@@ -33,36 +33,56 @@ const TitleComp = ({ title, description, type }) => {
 
 
   return (
-    <section className={`flex flex-col  w-full items-start justify-center ${description !== '' ? 'mb-8' : 'mb-4'}`} >
+    <div className={` ${styles.section_title_container} ${description !== '' ? 'mb-8' : 'mb-4'}`} >
 
-      <div className="flex flex-row items-center justify-center w-full mb-2 font-black text-xl normal-case sm:text-xl md:text-xl lg:text-2xl xl:text-3xl ">
-        <div className="flex flex-col w-auto">
+      <div className={`${styles.title_container}`}>
+        <div className={`${styles.title_split}`}>
           <h5 className={`${type === 'dark' ? 'text-nena-menu-text' : 'text-white'} ${wordscant === 2 ? 'pr-1 pl-2' : 'pl-2 pr-0'}`}>
             {words[0]}
           </h5>
-          <div className="flex flex-row items-center w-full px-0 -mt-1">
-            <div className={`flex w-full h-2 border-b-[0.4rem] rounded-bl-lg ${type === 'dark' ? 'border-nena-menu-text' : 'border-white'}`}>&nbsp;</div>
-            <div className={`flex w-4 h-3 mt-1 transform scale-50  rounded-full ${type === 'dark' ? 'bg-nena-menu-text' : 'bg-white'}`}></div>
+          <div className={`${styles.border_split}`}>
+            <span className={`rounded-bl-lg ${styles.border_left}
+            ${type === 'dark'
+                ? 'border-nena-menu-text'
+                : 'border-white'}`}>
+              &nbsp;</span>
+            <span className={`${styles.border_right}
+            ${type === 'dark'
+                ? 'bg-nena-menu-text'
+                : 'bg-white'}`}></span>
           </div>
         </div>
-        <div className="flex flex-col w-auto">
-          <h5 className={`flex w-auto ${type === 'dark' ? 'text-nena-secondary' : 'text-white'} ${wordscant === 2 ? 'pr-2 pl-1' : 'pl-0 pr-2'}`}>
+
+        <div className={`${styles.title_split}`}>
+          <h5 className={`
+           ${type === 'dark'
+              ? 'text-nena-secondary'
+              : 'text-white'}
+            ${wordscant === 2
+              ? 'pr-2 pl-1'
+              : 'pl-0 pr-2'}`}>
             {words[1]}
           </h5>
-          <div className="flex flex-row items-center w-full px-0 -mt-1">
-            <div className={`flex w-4 h-3 mt-1 transform scale-50 rounded-full ${type === 'dark' ? 'bg-nena-secondary' : 'bg-white'}`}></div>
-            <div className={`flex w-full h-2 border-b-[0.4rem] rounded-br-lg ${type === 'dark' ? 'border-nena-secondary' : 'border-white'}`}>&nbsp;</div>
+          <div className={`${styles.border_split}`} >
+            <div className={` ${styles.border_right}
+              ${type === 'dark'
+                ? 'bg-nena-secondary'
+                : 'bg-white'}`}></div>
+            <div className={`rounded-br-lg ${styles.border_left}
+            ${type === 'dark'
+                ? 'border-nena-secondary'
+                : 'border-white'}`}>&nbsp;</div>
           </div>
         </div>
       </div>
 
-      <div className="flex w-full flex-row justify-center items-center px-10 md:px-10 xl:px-52 font-semibold" >
-        <p className={`flex ${type === 'dark' ? 'text-gray-500' : 'text-white'} text-normal  text-sm md:text-base text-center `}>
+      <div className={`${styles.paragraph_container}`} >
+        <p className={` ${type === 'dark' ? 'text-gray-500' : 'text-white'} `}>
           {description}
         </p>
       </div>
 
-    </section>
+    </div>
   )
 }
 
