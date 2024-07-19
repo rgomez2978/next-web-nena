0223f4cf103bfbdb3bf292df0177e806c14078f9
Ronald Gomez <ronald.gomez@dronena.com>
2024-07-19 15:04:37 -0400

    add export dist

--- src/components/modules/SectionOurHistory/index.jsx
+++ src/components/modules/SectionOurHistory/index.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+'use client';
+import { useEffect, useState, useRef } from 'react'
+import useIntersectionObserver from "@/hooks/useIntersectionObserver";
+
 import { Title } from '@/components/shared'
 import styles from "./style.module.scss";
 
 const SectionOurHistory = ({ data }) => {
+  const myRefFade = useRef();
+  const myRefLeft = useRef();
+  const myRefRight = useRef();
+  useIntersectionObserver(myRefFade, 'animate-fade');
+  useIntersectionObserver(myRefLeft, 'animate-fade-left');
+  useIntersectionObserver(myRefRight, 'animate-fade-right');
+
+
+
   if (data && data.length > 0) {
     const { theme, title, items } = data[0]
 
@@ -18,10 +30,8 @@ const SectionOurHistory = ({ data }) => {
             <div key={id} class={`${styles.list_items_content} ${index % 2 === 0 && 'flex-row md:flex-row-reverse'}`}>
               <div className={`${styles.empty_space}`}></div>
               <div className={`${styles.bullet_year}`}
-                data-aos="fade-in"
-                data-aos-duration="300"
-                data-aos-easing="linear"> <h1>{year}</h1> </div>
-              <div className={`${styles.item_content} `} data-aos={`${index % 2 === 0 ? 'fade-right' : 'fade-left'}`} >
+                ref={myRefRight}> <h1>{year}</h1> </div>
+              <div className={`${styles.item_content} `} ref={index % 2 === 0 ? myRefRight : myRefLeft} >
                 <h3>{title}</h3>
                 <p>{description}</p>
               </div>
